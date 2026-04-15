00266855e6e670de14fb95238cf97a7be9866afa
shwatana <shwatana@C010423733.zipangu.cac.co.jp>
2026-04-15 17:35:23 +0900

    テスト送信

--- Java100_questions_cac2nd/src/lesson01/challenge01/Patisserie.java
+++ Java100_questions_cac2nd/src/lesson01/challenge01/Patisserie.java
@@ -18,7 +18,7 @@ package lesson01.challenge01;
 public class Patisserie {
 
 	public static void main(String[] args) {
-
+		//a
 		System.out.println("たいへんお待たせいたしました");
 		System.out.println("【ポエール・ネルメ】");
 		System.out.println("ただいまより開店です!!");
